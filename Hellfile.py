ea578f75b628fd864d249239109ea81785da8ade
Jack Jennings <j@ckjennin.gs>
2026-05-09 12:17:54 -0700

    refactor: replace local chutes with packages, remove woff support

--- Hellfile.py
+++ Hellfile.py
@@ -1,14 +1,8 @@
 from hellbox import Hellbox
 from hellbox.jobs.dsig import InsertDummyDsig
 from hellbox.jobs.fontmake import GenerateOtf, GenerateTtf
-
-from lib.autohint import Autohint
-from lib.woff import GenerateWoff
-from lib.woff2 import GenerateWoff2
-
-
-PostProduction = Hellbox.compose(InsertDummyDsig(), Autohint())
-
+from hellbox.jobs.ttfautohint import Autohint
+from hellbox.jobs.woff2 import GenerateWoff2
 
 Hellbox.default = "build"
 
@@ -16,12 +10,7 @@ with Hellbox("build") as task:
     task.describe("Builds font files from source")
 
     source = task.read("source/*.ufo")
-    otf = source >> GenerateOtf() >> PostProduction()
-    ttf = source >> GenerateTtf() >> PostProduction()
-    woff = ttf >> GenerateWoff()
-    woff2 = ttf >> GenerateWoff2()
-
-    otf >> task.write("build/otf")
+    source >> GenerateOtf() >> InsertDummyDsig() >> Autohint() >> task.write("build/otf")
+    ttf = source >> GenerateTtf() >> InsertDummyDsig() >> Autohint()
     ttf >> task.write("build/ttf")
-    woff >> task.write("build/woff")
-    woff2 >> task.write("build/woff2")
+    ttf >> GenerateWoff2() >> task.write("build/woff2")
--- README.md
+++ README.md
@@ -1,23 +1,12 @@
 # HJ Round
 
-This project is a demonstration of how hellbox builds font files.
+A demonstration of how hellbox builds font files.
 
-To try this project:
+## Usage
+
 1. Clone this repository
-2. Install the [`hell`](https://github.com/hellboxpy/hell) CLI
-3. Install [`pipenv`](https://pipenv.readthedocs.io/en/latest/#install-pipenv-today)
-4. Run `hell install` to get the python dependencies
-5. Run `hell run` to build the font files
+2. Install the [`hell`](https://github.com/hellboxpy/hell#installation) CLI
+3. Run `uv sync` to install dependencies
+4. Run `hell run` to build the font files
 
-## Dependencies
-
-A number of external dependencies are currently required. To get the with Homebrew, run the following commands:
-
-```shell
-brew tap bramstein/webfonttools
-brew install sfnt2woff-zopfli
-brew install woff2
-brew install ttfautohint
-```
-
-Eventually these should be bundled with their respective chutes to the extent possible.
+Build output is written to `build/otf`, `build/ttf`, and `build/woff2`.
